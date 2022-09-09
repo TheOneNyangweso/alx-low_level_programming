334390d8e6d39322978e514f82e0b72f9fb44f70
TheOneNyangweso <morurisammy5@gmail.com>
2022-09-09 22:28:54 +0000

    "Task 4"

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main- main function loops through lowercase a up to lowercase z
+ * and outputs the results except letters q and e
+ * Return: null i.e 0
+ */
+
+int main(void)
+{
+	/* Variable initialization */
+	char a = 'a';
+
+	/* Looping through the ASCII characters of a-z */
+	while (a <= 'z')
+	{
+		if (a != 'e' && a != 'q')
+			putchar(a);
+		else
+			continue;
+		a++;
+	}
+	putchar('\n');
+
+	return (0);
+}
