36d09a5ccebcbc5652a5da61b883258c51d49fda
TheOneNyangweso <morurisammy5@gmail.com>
2022-09-09 22:05:00 +0000

    "Task 2"

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/*
+ * main: function main loops through lowercase a up to lowercase z
+ * Return: null
+ */
+
+int main(void)
+{
+	/* Variable initialization */
+	char a = 'a';
+
+	/* Looping through the ASCII characters of a-z */
+	while (a <= 'z')
+		putchar(a);
+	putchar('\n');
+
+	return 0;
+}
