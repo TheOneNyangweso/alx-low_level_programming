793add932af1e7b434e0b980123a9251b3baa88b
TheOneNyangweso <morurisammy5@gmail.com>
2022-09-09 22:40:39 +0000

    "Task 5"

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main- main function prints all single digit numbers from 0-9
+ * and outputs the results
+ * Return: null i.e 0
+ */
+
+int main(void)
+{
+	/* Variable initialization */
+	int a = 0;
+
+	/* Looping through 0-9 */
+	while (a <= 9)
+	{
+		printf("%d", a);
+		a++;
+	}
+	putchar('\n');
+
+	return (0);
+}
