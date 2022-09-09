0f43aca97354c5b882a70ae4239ff918d42f8575
TheOneNyangweso <morurisammy5@gmail.com>
2022-09-09 21:30:07 +0000

    "Task 1"

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main- main function generates random number and checks if last digit
+ * of the latter satisfies the given condition
+ * Return: null i.e  0
+ */
+int main(void)
+{
+	int n;
+	String s = "Last digit of ";
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	a = n % 10;
+	if (a > 5)
+		printf("%s %d is %d and is greater than 5\n", s, n, a);
+	if (a == 0)
+		printf("%s %d is %d and is 0\n", s, n, a);
+	if (a < 6)
+		printf("%s %d is %d and is less than 6\n", s, n, a);
+	return (0);
+}
