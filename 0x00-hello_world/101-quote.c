6f449b6a66efc31f85fed4343f0d08aebbba9b54
TheOneNyangweso <morurisammy5@gmail.com>
2022-09-10 09:14:29 +0000

    "Task 101-qoute.c"

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio/h>
+
+/**
+ * main- the main function prints the output as per the instructions
+ * given
+ * Return: 1
+ */
+int main(void)
+{
+	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19")
+	return (1);
+}
