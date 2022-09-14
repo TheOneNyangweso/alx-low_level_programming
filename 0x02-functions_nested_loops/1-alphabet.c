023ef52e5d1f99e4a2adbf475422b1c0e8cfa3cb
TheOneNyangweso <morurisammy5@gmail.com>
2022-09-14 08:09:25 +0000

    "1-alphabet.c"

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * main- prints lowercase alphabets  followed by newline.
+ * Return: 0 i.e null after successful completion.
+ */
+int main(void)
+{
+	char letters;
+	char a[];
+
+	letters =  'a';
+
+	while (letters <= 'z')
+	{
+		_putchar(a[letters]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 /**
- * Header file for 0x02-functions_nested_loops
+ *Header file for 0x02-functions_nested_loops projects
  */
 
 int _putchar(char c);
+void print_alphabet(void);
