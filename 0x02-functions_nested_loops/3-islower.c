c7fb1bb9b21c17f74822d1350cf25d8e90669b01
TheOneNyangweso <morurisammy5@gmail.com>
2022-09-14 10:34:16 +0000

    "task 3"

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,18 @@
 #include <unistd.h>
 #include "main.h"
 
+/**
+ * _islower-checks if char variable c is lower.
+ * int c- parameter for c
+ * Return: 1 if c is lowercase else return is set to 0
+ */
+
 int _islower(int c)
 {
-	char a, b;
+	int a;
 	a = _islower(int c);
 
-	if (a == ('a'<= b >= 'z'))
+	if (int('a') <= a && a >= int('z'))
 		return (1);
 	else
 		return (0);
