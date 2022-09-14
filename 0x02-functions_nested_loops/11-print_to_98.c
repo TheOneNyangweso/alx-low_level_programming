2303cc68156c1cc6dc0c3cd6be7df2ca208d1400
TheOneNyangweso <morurisammy5@gmail.com>
2022-09-14 23:37:58 +0000

    "print_to_98"

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,12 +2,19 @@
 #include <unistd.h>
 #include "main.h"
 
+/**
+ * print_to_98 - outputs n to 98.
+ * @n: parameter for accepting int variable n.
+ *
+ * Return: all natural numbers from n to 98.
+ */
+
 void print_to_98(int n)
 {
 	int a;
 
 	a = n;
-	while(a <= 98)
+	while (a <= 98 || 98 <= a)
 	{
 		printf("%d, ", a);
 		a++;
