5751f05d86d0f1c9663a0d7eb7b099d5781ff830
TheOneNyangweso <morurisammy5@gmail.com>
2022-09-14 21:40:41 +0000

    "6-abs.c"

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -5,14 +5,16 @@
 /**
  * _abs- function _abs prints the absolute number of an input
  * @a: parameter for check
- * Return: a, which the absolute value.
+ * Return: result, which the absolute value.
  */
 int _abs(int a)
 {
 	int result;
 
-	result = a * a;
-	a = result / a;
+	if (a < 0)	
+		result = a * -1;
+	else
+		result = a;
 
-	return (a);
+	return (result);
 }
