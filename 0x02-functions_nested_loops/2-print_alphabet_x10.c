0f236a873832343d9ef204986e48a85e8ec90f26
TheOneNyangweso <morurisammy5@gmail.com>
2022-09-14 09:24:39 +0000

    "Task 2"

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_alphabet- prints lowercase alphabet 10 times, followed by newline.
+ * Return: 0 i.e null after successful completion.
+ */
+void print_alphabet_x10(void)
+{
+	int count;
+	char a;
+
+	count = 0;
+	while (count < 10)
+	{
+		a = 'a';
+		while (a <= 'z')
+		{
+			_putchar(a);
+			a++;
+		}
+		_putchar('\n');
+		count++;
+	}
+	_putchar('\n';)
+}
