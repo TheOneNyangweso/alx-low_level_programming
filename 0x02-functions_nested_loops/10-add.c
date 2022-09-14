ca40c09195389833cf2a8e23d3fc720b0df320b6
TheOneNyangweso <morurisammy5@gmail.com>
2022-09-14 23:07:39 +0000

    "10-add.c"

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * add- function adds two integers and outputs result
+ * @b: parameter for accepting first int.
+ * @c: parameter for accepting second int.
+ * Return: sum of b and c.
+ */
+
+int add(int b, int c)
+{
+	int result;
+
+	result = b + c;
+	return (result);
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,6 @@
+#include <unistd.h>
+#include "main.h"
+
+void jack_bauer(void)
+{
+}
