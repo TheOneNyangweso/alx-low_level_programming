68e2fec83f1a15a40b388f19241ed7028df1e9a5
TheOneNyangweso <morurisammy5@gmail.com>
2022-09-15 23:52:05 +0000

    "6-print_line.c"

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_line- function for printline
+ * @n: parameter for print_line
+ * Return: null
+ */
+
+void print_line(int n)
+{
+	int count;
+
+	if (n <= 0)
+		_putchar('\n');
+	count = 1;
+	while (count <= n)
+	{
+		_putchar('_');
+		count++;
+	}
+}
