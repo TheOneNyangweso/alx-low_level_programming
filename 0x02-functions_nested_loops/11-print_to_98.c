8ced68f10f0f6cb57afdd14b2182d71b7ea8b49f
TheOneNyangweso <morurisammy5@gmail.com>
2022-09-15 20:41:41 +0000

    "task 11"

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -18,7 +18,7 @@ void print_to_98(int n)
 			printf("%d, ", n);
 		else
 		{
-			printf("%d ", n);
+			printf("%d\n", n);
 			exit (0);
 		}
 		n--;
@@ -28,8 +28,7 @@ void print_to_98(int n)
 		if (n != 98)
 			printf("%d, ", n);
 		else
-			printf("%d ", n);
+			printf("%d\n", n);
 		n++;
 	}
-	_putchar('\n');
 }
