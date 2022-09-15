20eb63497db31e67d0d1e1fffd0b02a259e66e9d
TheOneNyangweso <morurisammy5@gmail.com>
2022-09-15 21:59:22 +0000

    "0-isupper"

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _isupper- function checks if char input is uppercase
+ * @c: parameter variable for _isupper
+ * Return: c if input is uppercase, else return 0.
+ */
+
+int _isupper(int c);
+{
+	if (c >= 'A' && c <= 'Z')
+		return (c);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,7 @@
+/**
+ * Header file for 0x04-more_functions_nested_loops projects
+ */
+
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
