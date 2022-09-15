86bec0b5f33ae405882d1bae0dee6b361ae86ee8
TheOneNyangweso <morurisammy5@gmail.com>
2022-09-15 20:55:42 +0000

    "task 100"

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,43 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_times_table- function that prints multiplication table of 0 <= n <= 15
+ * @n: parameter requires no specific data type
+ * Return: void.
+ */
+
+void print_times_table(int n)
+{
+	int a, b, c;
+
+	a = 0;
+
+	while (a <= n)
+	{
+		_putchar('0');
+		b = 1;
+		while (b < 10)
+		{
+			c = a * b;
+
+			if (c <= 9)
+			{
+				_putchar(',');
+				_putchar(32);
+				_putchar(32);
+				_putchar(c + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(32);
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+			}
+			b++;
+		}
+		_putchar('\n');
+		a++;
+	}
+}
