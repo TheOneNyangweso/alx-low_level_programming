deaf1df2cebc6a6115658431b3bfcc67ca95134d
TheOneNyangweso <morurisammy5@gmail.com>
2022-09-15 23:37:45 +0000

    "5-more_numbers"

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * more_numbers- function prints numbers from 0 to 14, with
+ * the no exception(s)
+ * Return: null.
+ */
+
+void more_numbers(void)
+{
+	int a, count;
+
+	count = 0;
+	while (count < 10)
+	{
+		a = 0;
+		while (a < 15)
+		{
+			if (a >= 10)
+				_putchar((a / 10) + '0');
+			_putchar((a %  10) + '0');
+			a++;
+		}
+		_putchar('\n')
+		count++;
+	}
+}
