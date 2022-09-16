78bb3b7f1133855c4c0042f2b8ac0fb65dcae1b9
TheOneNyangweso <morurisammy5@gmail.com>
2022-09-16 11:20:13 +0000

    "9-fizz_buzz.c It works!!"

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -19,12 +19,12 @@ int main(void)
 		b = a % 3;
 		c = a % 5;
 		d = b + c;
-		if (b == 0)
+		if (d == 0)
+			printf("%s ", f_buzz);
+		else if (b == 0)
 			printf("%s ", fizz);
 		else if (c == 0)
 			printf("%s ", buzz);
-		else if (d == 0)
-			printf("%s ", f_buzz);
 		else
 			printf("%d ", a);
 	}
