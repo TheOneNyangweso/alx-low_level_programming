f325515987c92862ae620b7511de93d34614213e
TheOneNyangweso <morurisammy5@gmail.com>
2022-09-20 00:41:03 +0000

    "3-puts.c"

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts - function prints string, followef by newline. Similar
+ * to puts function in standard library.
+ * @str: pointer character variable
+ */
+void _puts(char *str)
+{
+	int a, count, result = 0;
+
+	count = 0;
+	while (*(str + count) != '\0')
+	{
+		result = count + 1;
+		count++;
+	}
+
+	for (a = 0; a <= result; a++)
+		_putchar(str[0]);
+	_putchar('\n')
+}
