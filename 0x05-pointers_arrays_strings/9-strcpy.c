ecb310bb2445ebbfd4724dde4d85ce4e6a78ff7d
TheOneNyangweso <morurisammy5@gmail.com>
2022-09-20 20:37:12 +0000

    "9-strcpy"

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcpy - function which performs similar function as
+ * strcpy () function in the standard library
+ * @dest: array referenced by *dest
+ * @src: content of string to be copied
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (*(src + i))
+	{
+		*(dest + i) = *(src + i);
+	}
+	*(dest + i) = '\0';
+
+	return (dest);
+}
