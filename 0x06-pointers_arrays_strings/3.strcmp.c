bfec2df23b90efaed0417ddc83c41c7b33ccd74e
TheOneNyangweso <morurisammy5@gmail.com>
2022-09-22 23:56:45 +0000

    "3-strcmp to be finished"

---
+++ 0x06-pointers_arrays_strings/3.strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: string argument
+ * @s2: string argument
+ * Return: an integer depending on the result, in accordance
+ * to the man strcmp page.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int a;
+
+	a = 0;
+
+}
