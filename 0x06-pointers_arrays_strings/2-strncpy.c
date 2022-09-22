e17791be1476f47cd94b1d6942491a84397c1c3c
TheOneNyangweso <morurisammy5@gmail.com>
2022-09-22 23:21:20 +0000

    "2-strncpy.c"

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies strings
+ * @dest: src string argument
+ * @src: dest string argument
+ * @n: test integer
+ * Return: pointer to the resulting strinf dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a, b, count;
+
+	for (count = 0; *(dest + count) != '\0'; count++)
+		;
+	for (b = 0; *(src + b); b++)
+		;
+
+	for (a = 0; a < b && a < n && src[a] != '\0'; a++)
+	{
+		*(dest + count) = *(src + a);
+		count++;
+	}
+
+	while (a < n)
+	{
+		dest[count] = '\0;
+		a++;
+	}
+
+	return (dest);
+}
