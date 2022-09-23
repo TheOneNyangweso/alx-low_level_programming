35de49fca8e7512d7e5a9075100ef43e079c079a
TheOneNyangweso <morurisammy5@gmail.com>
2022-09-23 19:34:43 +0000

    "101-print_number.c"

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_number - function prints numbers using _putchar only
+ * The concept of recursion is applied here
+ * @n: argument where int variable is passed to
+ * Return: null
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n == 0)
+		_putchar('0');
+
+	if (n / 10)
+		_print_number(n / 10);
+
+	putchar(n % 10 + '0');
+
+}
