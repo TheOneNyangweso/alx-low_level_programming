b47f6d9222e1f37bb5f68164ea04256011a86028
TheOneNyangweso <morurisammy5@gmail.com>
2022-09-27 01:55:06 +0000

    "2-strchr.c"

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strchr - function similar to strchr function in standard library
+ * @s: returns pointer to c
+ * @c: char to be searched
+ * Return: a pointer to first occurence
+ */
+
+char *_strchr(char *s, char c)
+{
+}
