be361543774fb9166a76680f3501405751a671b0
TheOneNyangweso <morurisammy5@gmail.com>
2022-10-28 10:08:05 +0000

    "100-print_comb3.c It works!1"

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - function prints combination of no.s
+ * Return: null on success compilation
+ */
+int main(void)
+{
+	int a, b;
+
+	a = 0;
+
+	while (a <= 8)
+	{
+		b = 0;
+		while (b <= 9)
+		{
+			if (b != a)
+			{
+				if (((b + a) == (a + b)) && (a < b))
+				{
+					putchar(a + '0');
+					putchar(b + '0');
+					if (!(a == 8 && b == 9))
+						putchar(',');
+					putchar(' ');
+				}
+			}
+			b++;
+		}
+		a++;
+	}
+	putchar('\n');
+
+	return (0);
+}
